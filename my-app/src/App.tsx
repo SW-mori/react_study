e38b491bea93bea7cf8d70f8deebd913fc261515
SW-mori <tmori@skywill.jp>
2022-07-25 18:13:55 +0900

    App.tsxリファクタリング

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,54 +1,15 @@
 import './css/App.css';
 import Title from './components/title';
-import Button from './components/button/setCountBtn';
-import { useState } from 'react';
+import Counter from './components/counter/counter';
 
-const Counter = () => {
-  // open という名前の state 変数を宣言、初期値 true をセット
-  const [open, setOpen] = useState(true)
-  // toggleの関数を宣言
-  const toggle = () => setOpen(!open)
-  // countという名前のstate変数を宣言、初期値0をセット
-  const [count, setCount] = useState(0)
-
-  const addCount = () => {
-    setCount(prevState => prevState + 1)
-  }
-  
-  const minusCount = () => {
-    setCount(count - 1)
-  }
-
-  const counterReset = () => {
-    setCount(0)
-  }
-
-  const data = {
-    add: {
-      text: '+ 1',
-      value: addCount
-    },
-    minus: {
-      text: '- 1',
-      value: minusCount
-    },
-    reset: {
-      text: 'reset',
-      value: counterReset
-    }
-  }
+const App = () => {
 
   return (
     <div className="App">
       <Title/>
-      <button onClick={toggle}>{open ? 'close' : 'open'}</button>
-      <div className={open ? 'isOpen' : 'isClose'}>
-        <p>現在の数字は{count}です</p>
-        <Button data={data.add}/>
-        <Button data={data.minus}/>
-        <Button data={data.reset}/>
-      </div>
+      <Counter/>
     </div>
   )
 }
-export default Counter;
+
+export default App;
