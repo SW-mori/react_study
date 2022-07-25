46f63537849d4854109da43934e2ddab19344594
SW-mori <tmori@skywill.jp>
2022-07-25 18:07:41 +0900

    カウンター処理をコンポーネント化

---
+++ my-app/src/components/counter/counter.tsx
@@ -0,0 +1,61 @@
+import '../../css/counter.css';
+import Button from '../../components/button/setCountBtn';
+import { useState } from 'react';
+
+const Counter = () => {
+  
+  // open という名前の state 変数を宣言、初期値 true をセット
+  const [open, setOpen] = useState(true)
+
+  // toggleの関数を宣言
+  const toggle = () => setOpen(!open)
+
+  // countという名前のstate変数を宣言、初期値0をセット
+  const [count, setCount] = useState(0)
+
+  // カウントを+1する関数addCountを宣言
+  const addCount = () => {
+    setCount(prevState => prevState + 1)
+  }
+
+  // カウントを-1する関数minusCountを宣言
+  const minusCount = () => {
+    setCount(prevState => prevState - 1)
+  }
+
+  // カウントを0にリセットする関数counterResetを宣言
+  const counterReset = () => {
+    setCount(0)
+  }
+
+
+  // テキストと関数をもつオブジェクトdataを定義
+  const data = {
+    add: {
+      text: '+ 1',
+      value: addCount
+    },
+    minus: {
+      text: '- 1',
+      value: minusCount
+    },
+    reset: {
+      text: 'reset',
+      value: counterReset
+    }
+  }
+
+  return (
+    <>
+      <button onClick={toggle}>{open ? 'close' : 'open'}</button>
+      <div className={open ? 'isOpen' : 'isClose'}>
+        <p>現在の数字は{count}です</p>
+        <Button data={data.add}/>
+        <Button data={data.minus}/>
+        <Button data={data.reset}/>
+      </div>
+    </>
+  )
+}
+
+export default Counter;
