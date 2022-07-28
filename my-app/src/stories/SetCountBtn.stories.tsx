36675a23466ddb51a6d6b977c1442979bd8eb958
SW-mori <tmori@skywill.jp>
2022-07-28 17:46:06 +0900

    countボタン story作成

--- my-app/src/stories/SetCountBtn.stories.tsx
+++ my-app/src/stories/SetCountBtn.stories.tsx
@@ -1,16 +1,31 @@
+import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Button } from '../components/button/setCountBtn';
+
+import { Button } from './SetCountBtn';
 
 export default {
   title: 'Example/SetCountBtn',
   component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const CountUp = Template.bind({});
-export const CountDown = Template.bind({});
-export const Reset = Template.bind({});
-
 CountUp.args = {
+  primary: true,
+  label: '+ 1',
+};
+
+export const CountDown = Template.bind({});
+CountDown.args = {
+  label: '- 1',
+};
+
+export const Reset = Template.bind({});
+Reset.args = {
+  size: 'large',
+  label: 'reset',
 };
---
+++ my-app/src/stories/SetCountBtn.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// import './setCountBtn.css';
+
+interface ButtonProps {
+  primary?: boolean;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
